01cf81d957485a8a10c118b89424faa9e4a622cd
kou256 <25428510+kou256@users.noreply.github.com>
2023-06-15 22:32:33 +0900

    :sparkles: カメラからの映像の表示
    SkyWay経由で映像を取得・表示する機能を実装した。

---
+++ src/components/BaseVideo.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted } from "vue";
+import { SkyWayStreamFactory } from "@skyway-sdk/room";
+
+const initVideo = async () => {
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    const { audio, video } =
+      await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream();
+    const localVideo = document.getElementById("local-video");
+    video.attach(localVideo);
+    await localVideo.play();
+  }
+};
+
+onMounted(() => {
+  initVideo();
+});
+</script>
+
+<template>
+  <video id="local-video" playsinline></video>
+</template>
+
+<style scoped></style>
