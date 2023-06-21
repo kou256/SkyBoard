419a26bb97a29265658ecdbde1a28bf030b16283
kou256 <25428510+kou256@users.noreply.github.com>
2023-06-21 12:48:17 +0900

    :sparkles: ブラシの幅調整
    ペイントに使用するブラシの幅を調整できるようにした。ついでにアイコンが読み込まれるように修正した。

--- src/components/PublisherCanvasCtrl.vue
+++ src/components/PublisherCanvasCtrl.vue
@@ -1,22 +1,24 @@
 <script setup>
-import {ref} from "vue";
-const colorPicker = ref(null);
-const paintMode = ref(null);
+import { ref } from "vue";
+const colorPicker = ref("#000000");
+const paintMode = ref(0);
 const paintModes = ["cursor", "pencil", "eraser"];
-const overlay = ref(false);
+const isColorPickerDialog = ref(false);
+const brushSize = ref(5);
 
-const emit = defineEmits(["changeColor", "changeMode"]);
-
-const onChangePaintMode = (modeIndex) => {
-  emit("changeMode", paintModes[modeIndex]);
-};
+const emit = defineEmits(["changeColor", "changeMode", "changeBrushSize"]);
 </script>
 
 <template>
   <div>
-    <v-btn-toggle v-model="paintMode" @update:model-value="onChangePaintMode">
+    <v-btn-toggle
+      v-model="paintMode"
+      variant="outlined"
+      mandatory
+      @update:model-value="emit('changeMode', paintModes[paintMode])"
+    >
       <v-btn icon>
-        <v-icon>mdi-cursor</v-icon>
+        <v-icon>mdi-cursor-pointer</v-icon>
       </v-btn>
       <v-btn icon>
         <v-icon>mdi-pencil</v-icon>
@@ -25,15 +27,33 @@ const onChangePaintMode = (modeIndex) => {
         <v-icon>mdi-eraser</v-icon>
       </v-btn>
     </v-btn-toggle>
-    <v-btn icon @click="overlay = !overlay">
-      <v-icon>mdi-palette</v-icon>
-    </v-btn>
-    <v-overlay v-model="overlay" @click:outside="overlay = false">
-      <v-color-picker v-model="colorPicker" @update:model-value="emit('changeColor', colorPicker)"/>
-    </v-overlay>
+    <v-dialog v-model="isColorPickerDialog" width="auto">
+      <template v-slot:activator="{ props }">
+        <v-btn icon v-bind="props" variant="outlined" rounded="xs">
+          <v-icon color="light-blue">mdi-palette</v-icon>
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-item>
+          <v-color-picker
+            v-model="colorPicker"
+            @update:model-value="emit('changeColor', colorPicker)"
+          />
+        </v-card-item>
+        <v-card-actions>
+          <v-btn block @click="isColorPickerDialog = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-slider
+      v-model="brushSize"
+      thumb-label
+      step="1"
+      min="1"
+      max="10"
+      @update:model-value="emit('changeBrushSize', brushSize)"
+    ></v-slider>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
